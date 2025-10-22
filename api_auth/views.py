1e7e480dfdfe0f8ad20d5623473c078ce761487c
believeriver <nobuyuki.galois@gmail.com>
2025-10-23 04:50:50 +0900

    20251023 01 edited views.py

--- api_auth/views.py
+++ api_auth/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Profile
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    """ Everyone can access to create user.-> AllowAny
+        only create method: generics.CreateAPIView
+    """
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny, )
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    CRUD already defined: viewsets.ModelViewSet
+    """
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        """When creating a user, override the perform_create method
+            to automatically assign the logged-in user from the request
+            on the API side."""
+        serializer.save(userProfile=self.request.user)
+
+
+class MyProfileListView(generics.ListAPIView):
+    """Return the profile of the logged-in user
+       only get method: generics.ListAPIView
+    """
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
--- api_insta/views.py
+++ api_insta/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from . import serializers
+from .models import InstaPost, InstaComment
 
-# Create your views here.
+
+class InstaPostViewSet(viewsets.ModelViewSet):
+    queryset = InstaPost.objects.all()
+    serializer_class = serializers.InstaPostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userPost=self.request.user)
+
+
+class InstaCommentViewSet(viewsets.ModelViewSet):
+    queryset = InstaComment.objects.all()
+    serializer_class = serializers.InstaCommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userComment=self.request.user)
+
