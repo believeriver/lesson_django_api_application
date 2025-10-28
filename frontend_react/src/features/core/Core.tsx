36d3ebc040db17a9075e1bf25ec78e36fccd973f
believeriver <nobuyuki.galois@gmail.com>
2025-10-29 06:19:57 +0900

    20251029 02 create Core.module.css and edit Core.tsx

--- frontend_react/src/features/core/Core.tsx
+++ frontend_react/src/features/core/Core.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  Button,
+  Grid,
+  Avatar,
+  Badge,
+  CircularProgress,
+  withStyles,
+} from '@mui/material';
+
+import { MdAddAPhoto } from 'react-icons/md'
+
+
 import Auth from '../auth/Auth';
+import styles from './Core.module.css';
+import type { AppDispatch } from '../../app/store';
 
 const Core: React.FC = () => {
   return (
     <div>
       <Auth />
+      <Button />
+
     </div>
   );
 };
