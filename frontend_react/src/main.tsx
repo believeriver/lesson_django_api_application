53bc1a1e2a0ab133e6df6ee4f6b3527acdf30c73
believeriver <nobuyuki.galois@gmail.com>
2025-11-03 07:48:42 +0900

    20251103 01 start to create new app.

--- frontend_react/src/main.tsx
+++ frontend_react/src/main.tsx
@@ -1,15 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { store } from './app/store.ts';
 import './index.css';
 import App from './App.tsx';
+import Menu from './features/front/Menu.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/insta_clone" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
