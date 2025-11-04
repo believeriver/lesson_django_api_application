8f90bda6a31f116a268c07df92f558dc4a2dd4cf
believeriver <nobuyuki.galois@gmail.com>
2025-11-05 06:31:32 +0900

    20251105 03 succes fetch household transactions.

--- frontend_react/src/features/household/HouseholdMain.tsx
+++ frontend_react/src/features/household/HouseholdMain.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import type { AppDispatch } from '../../app/store';
@@ -15,7 +15,16 @@ import type { Transaction } from './householdtypes';
 
 const HouseholdMain: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
-  const transactions = useSelector(selectTransactions)
+  const transactions = useSelector(selectTransactions);
+  useEffect(() => {
+    const fetchHouseholdBootLoader = async () => {
+      if (localStorage.localJWT) {
+        const result = await dispatch(fetchAsyncGetHouseholdTransactions());
+      }
+    };
+    fetchHouseholdBootLoader();
+  }, [dispatch]);
+  console.log('[INFO] household transactions:', transactions);
   return <div>HouseholdMain</div>;
 };
 
