0c3420babef9ed2be23d12799db274be0fff06dd
believeriver <nobuyuki.galois@gmail.com>
2025-11-05 20:48:05 +0900

    20251105 04 login menu before accessing household page.

--- frontend_react/src/features/household/HouseholdMain.tsx
+++ frontend_react/src/features/household/HouseholdMain.tsx
@@ -11,21 +11,53 @@ import {
   selectTransactions,
 } from './householdSlice';
 
+import {
+  setOpenSignIn,
+  resetOpenSignIn,
+  fetchAsyncGetMyProf,
+  selectMyProfile,
+  selectIsLoadingAuth,
+} from '../auth/authSlice';
+
 import type { Transaction } from './householdtypes';
+import Auth from '../auth/Auth';
 
 const HouseholdMain: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const transactions = useSelector(selectTransactions);
+  const profile = useSelector(selectMyProfile);
+  const isLoadingAuth = useSelector(selectIsLoadingAuth)
+  const isLoadingHousehold = useSelector(selectIsLoadingHousehold)
+
   useEffect(() => {
     const fetchHouseholdBootLoader = async () => {
+      //ログイン画面を閉じる>SNS cloneで定義ずみのため、ここでuseSelectorは使わない
+      // dispatch(resetOpenSignIn());
       if (localStorage.localJWT) {
-        const result = await dispatch(fetchAsyncGetHouseholdTransactions());
+        const result = await dispatch(fetchAsyncGetMyProf());
+        if (fetchAsyncGetMyProf.rejected.match(result)) {
+          //データの取得に失敗したら、ログイン画面に戻る
+          // dispatch(setOpenSignIn());
+          return null;
+        }
+        //ログインに成功したら、Transactionデータの取得をする
+        await dispatch(fetchAsyncGetHouseholdTransactions());
       }
     };
     fetchHouseholdBootLoader();
   }, [dispatch]);
   console.log('[INFO] household transactions:', transactions);
-  return <div>HouseholdMain</div>;
+
+  return (
+    <div>
+      <Auth />
+      {profile.nickName ? (
+        <p>Success. Household</p>
+      ) : (
+        <p>Login False</p>
+      )}
+    </div>
+  );
 };
 
 export default HouseholdMain;
