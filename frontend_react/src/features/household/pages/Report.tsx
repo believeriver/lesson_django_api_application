ec0fcc04926e0bcb57eefe8555e65bb488a733d4
believeriver <nobuyuki.galois@gmail.com>
2025-11-26 11:12:30 +0900

    20251126 02

--- frontend_react/src/features/household/pages/Report.tsx
+++ frontend_react/src/features/household/pages/Report.tsx
@@ -1,13 +1,50 @@
-import { Grid, Paper } from '@mui/material'
-import React from 'react'
+import { Grid, Paper } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Report = () => {
+import type { AppDispatch } from '../../../app/store';
+import {
+  fetchAsyncGetHouseholdTransactions,
+  selectTransactions,
+} from '../householdSlice';
+import { fetchAsyncGetMyProf, selectMyProfile } from '../../auth/authSlice';
+import Auth from '../../auth/Auth';
+import Navigation from '../../front/Navigation';
+import { formatMonth } from '../../utils/formatting';
+
+const Report: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const transactions = useSelector(selectTransactions);
+  const profile = useSelector(selectMyProfile);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  // DatabaseからTransactionデータを取得
+  useEffect(() => {
+    const fetchHouseholdBootLoader = async () => {
+      if (localStorage.localJWT) {
+        const result = await dispatch(fetchAsyncGetMyProf());
+        if (fetchAsyncGetMyProf.rejected.match(result)) {
+          return null;
+        }
+        //ログインに成功したら、Transactionデータの取得をする
+        await dispatch(fetchAsyncGetHouseholdTransactions());
+      }
+      return null;
+    };
+    fetchHouseholdBootLoader();
+  }, [dispatch]);
+
+  //Transactionデータを１ヶ月でフィルタリングする
+  const monthlyTransactions = transactions.filter((transaction) => {
+    return transaction.date.startsWith(formatMonth(currentMonth));
+  });
+
   return (
     <Grid container spacing={2}>
       {/* 日付エリア */}
       Report
     </Grid>
-  )
-}
+  );
+};
 
-export default Report
+export default Report;
