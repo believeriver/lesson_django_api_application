a172c3c00e033ed35a53b6537edd2bc9726e8c0a
believeriver <nobuyuki.galois@gmail.com>
2025-11-30 07:41:55 +0900

    20251130 01 editting TransactionTable.tsx

--- frontend_react/src/features/household/components/TransactionTable.tsx
+++ frontend_react/src/features/household/components/TransactionTable.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from 'react';
 import { alpha, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
@@ -20,16 +20,80 @@ import { compareDesc, parseISO } from 'date-fns';
 import { useSelector } from 'react-redux';
 
 import { type Transaction } from '../types';
-import { selectIsLoadingHousehold } from '../householdSlice';
+import {
+  selectIsLoadingHousehold,
+  fetchAsyncDeleteHouseholdTransaction,
+} from '../householdSlice';
 import { financeCalculations } from '../../utils/financeCalculations';
 import { formatCurrency } from '../../utils/formatting';
 import IconComponents from './common/IconComponents';
 
-
-const TransactionTable = () => {
-  return (
-    <div>TransactionTable</div>
-  )
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
 }
 
-export default TransactionTable
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface TransactionTableHeadProps {
+  numSelected: number;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+//テーブルヘッド
+function TransactionTableHead(props: TransactionTableHeadProps) {
+  const { onSelectAllClick, numSelected, rowCount } = props;
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all deserts',
+            }}
+          />
+          <TableCell align="left">日付</TableCell>
+          <TableCell align="left">カテゴリ</TableCell>
+          <TableCell align="left">金額</TableCell>
+          <TableCell align="left">内容</TableCell>
+        </TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface TransactionTableToolbarProps {
+  numSelected: number;
+  onDelete: () => void;
+}
+
+const TransactionTable = () => {
+  return <div>TransactionTable</div>;
+};
+
+export default TransactionTable;
