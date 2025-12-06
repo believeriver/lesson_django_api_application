8a40fc616132674f644f811cae1acf7aac770b6b
believeriver <nobuyuki.galois@gmail.com>
2025-12-06 20:44:12 +0900

    20251206 01 editting api_irbank models.

--- api_irbank/serializers.py
+++ api_irbank/serializers.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, Financial
 
 
 class CompanySerializer(serializers.ModelSerializer):
-    model = Company
-    fields = (
-        'id',
-        'code',
-        'name',
-        'stock',
-        'dividend',
-        'dividend_rank',
-        'dividend_rank_updated',
-        'industry',
-        'description',
-        'per', 'psr', 'pbr')
+    class Meta:
+        model = Company
+        fields = (
+            'id', 'code', 'name', 'stock', 'dividend',
+            'dividend_rank', 'dividend_rank_updated',
+            'industry', 'description',
+            'per', 'psr', 'pbr')
+
+
+class FinancialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Financial
+        fields = ('id', 'code', 'sales', 'operating_margin', 'eps',
+                  'equity_ratio', 'operating_cash_flow',
+                  'cash_and_equivalents', 'dividend_per_share',
+                  'payout_ratio', 'fiscal_year')
 
