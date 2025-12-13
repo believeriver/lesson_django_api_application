6a39849d0c69d2a822e979b06d5b506d4675a4a5
believeriver <nobuyuki.galois@gmail.com>
2025-12-13 17:38:13 +0900

    20251213 03 remodel Finacials model

--- api_irbank/models.py
+++ api_irbank/models.py
@@ -1,14 +1,15 @@
 from django.db import models, transaction
 from typing import List, Dict
+import json
 
 
 class Company(models.Model):
     code = models.CharField(max_length=16, primary_key=True)
-    name = models.CharField(max_length=100)
-    stock = models.CharField(max_length=32)
-    dividend = models.FloatField()
-    dividend_rank = models.IntegerField()
-    dividend_update = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
+    stock = models.CharField(max_length=32, blank=True)
+    dividend = models.FloatField(blank=True, null=True, default=None)
+    dividend_rank = models.IntegerField(blank=True, null=True, default=0)
+    dividend_update = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
         db_table = "companies"  # __tablename__相当
@@ -91,8 +92,8 @@ class Company(models.Model):
 
 class Information(models.Model):
     company_code = models.CharField(max_length=16)
-    industry = models.CharField(max_length=10, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    industry = models.CharField(max_length=10, blank=True)
+    description = models.TextField(blank=True)
     per = models.FloatField(blank=True, null=True)
     psr = models.FloatField(blank=True, null=True)
     pbr = models.FloatField(blank=True, null=True)
@@ -125,6 +126,7 @@ class Information(models.Model):
 
 class Financial(models.Model):
     """
+    財務データ（会社×年度）
     1. sales: 売上高
     2. operating_margin: 営業利益率
     3. eps: EPS
@@ -136,31 +138,61 @@ class Financial(models.Model):
     9. fiscal_year: 会計年度
     """
     # code = models.ForeignKey(Company, on_delete=models.CASCADE, to_field='code')
-    company_code = models.CharField(max_length=20, db_index=True, default='', blank=True)
-    sales = models.DecimalField(max_digits=20, decimal_places=2, blank=True)
-    operating_margin = models.FloatField(blank=True)
-    eps = models.FloatField(blank=True)
-    equity_ratio = models.FloatField(blank=True)
-    operating_cash_flow = models.DecimalField(max_digits=20, decimal_places=2)
-    cash_and_equivalents = models.DecimalField(max_digits=20, decimal_places=2)
-    dividend_per_share = models.FloatField(blank=True)
-    payout_ratio = models.FloatField(blank=True)
-    fiscal_year = models.CharField(max_length=16)
+    company_code = models.CharField(max_length=16, verbose_name="会社コード", default='')
+    sales = models.CharField(max_length=32, blank=True, verbose_name="売上高")
+    operating_margin = models.FloatField(blank=True, null=True, verbose_name="営業利益率")
+    eps = models.FloatField(blank=True, null=True, verbose_name="EPS")
+    equity_ratio = models.FloatField(blank=True, null=True, verbose_name="自己資本比率")
+    operating_cash_flow = models.BigIntegerField(blank=True, null=True, verbose_name="営業CF")
+    cash_and_equivalents = models.BigIntegerField(blank=True, null=True, verbose_name="現金等")
+    dividend_per_share = models.BigIntegerField(blank=True, null=True, verbose_name="1株配当")
+    payout_ratio = models.FloatField(blank=True, null=True, verbose_name="配当性向")
+    # fiscal_year = models.CharField(max_length=16, blank=True, null=True, verbose_name="会計年度")
+    fiscal_year = models.CharField(max_length=16, verbose_name="会計年度")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
+        db_table = 'financials'
         unique_together = ['company_code', 'fiscal_year']  # 企業×年度でユニーク制約
-        indexes = [models.Index(fields=['company_code', 'fiscal_year'])]
-
-    @staticmethod
-    @transaction.atomic
-    def get_or_create_fast(code, fiscal_year, **data):
-        obj, created = Financial.objects.update_or_create(
-            company_code=code,
-            fiscal_year=fiscal_year,
-            defaults=data)
-        return obj
 
     def __str__(self):
-        return f'{self.company_code}: {self.fiscal_year}'
+        return f"{self.company_code} - {self.fiscal_year}"
+
+    @classmethod
+    def get_or_create_update(
+            cls, _code, _fiscal_year, _sale, _margin, _eps, _equity,
+            _cashflow, _equivalents, _dividend, _payout):
+        """Django版upsert（1クエリ）"""
+        obj, created = cls.objects.update_or_create(
+            company_code=_code,
+            fiscal_year=_fiscal_year,
+            defaults={
+                'sales': _sale,
+                'operating_margin': _margin,
+                'eps': _eps,
+                'equity_ratio': _equity,
+                'operating_cash_flow': _cashflow,
+                'cash_and_equivalents': _equivalents,
+                'dividend_per_share': _dividend,
+                'payout_ratio': _payout,
+            }
+        )
+        return obj
+
+    @classmethod
+    def get_financial_by_company_code(cls, code):
+        """単一取得（存在しない場合はNone）"""
+        try:
+            return cls.objects.get(company_code=code)
+        except cls.DoesNotExist:
+            return None
+
+    @classmethod
+    def get_financials_by_company_code(cls, code):
+        """複数取得（年度別リスト）"""
+        return list(cls.objects.filter(company_code=code)
+                    .values('company_code', 'sales', 'operating_margin',
+                            'eps', 'equity_ratio', 'operating_cash_flow',
+                            'cash_and_equivalents', 'dividend_per_share',
+                            'payout_ratio', 'fiscal_year'))
