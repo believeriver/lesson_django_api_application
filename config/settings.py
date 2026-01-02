b64aa9052ee4364767f4491a5782ba3bb47caee7
believeriver <nobuyuki.galois@gmail.com>
2026-01-02 13:59:15 +0900

    20260102 02 settings to .evn by environ

--- config/settings.py
+++ config/settings.py
@@ -13,22 +13,28 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+root = environ.Path(BASE_DIR / 'secrets')
+
+# Development
+env.read_env(root('.env.dev'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8oan2*gf@^elk)@uilon5c@=ttsuf3hq-#6q&a$d&h5#+8&s6j'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
